cf66d0f1216d1448bff28fb151c65f76a1a7ebb4
Zhansar <daid8002@gmail.com>
2024-10-02 12:41:34 +0500

    changed hero sections photo

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <section className="w-full items-center flex flex-col lg:mx-auto h-fit lg:items-start lg:max-w-6xl lg:py-10">
+      <section className="w-full  items-center flex flex-col lg:mx-auto h-fit lg:items-start lg:max-w-6xl p-10 bg-white mt-10 rounded-2xl shadow-xl">
         <div className="w-full flex md:flex-col justify-between items-center lg:flex-row">
           <div className="font-spaceGrotesk tracking-tight text-3xl font-semibold p-8 mt-10 lg:text-5xl lg:p-0">
-            <b className="font-bold text-4xl lg:text-6xl text-[#adeb1c] ">
+            <b className="font-bold text-4xl lg:text-6xl text-[#9193ff] ">
               BestOffer
             </b>{" "}
             <br />
             Финансовый маркетплейс для всех
           </div>
-          <div className="hidden lg:block">
+          <div className=" lg:block">
             <Image
-              src={"./Through the park cuate.svg"}
+              src={"./Business Team Illustration.svg"}
               width={500}
               height={500}
-              className="max-w-xl"
+              className="lg:max-w-xl md:max-w-[350px]"
               alt="park"
             />
           </div>

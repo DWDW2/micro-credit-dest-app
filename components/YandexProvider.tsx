add08e332789712c916243e5b9b666eb3f473247
Zhansar <daid8002@gmail.com>
2024-10-09 09:13:19 +0500

    fixed yandexx metrika

--- components/YandexProvider.tsx
+++ components/YandexProvider.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren, useCallback, useEffect } from "react";
 import ym, { YMInitializer } from "react-yandex-metrika";
 
 export const enableYM =
-  process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_YM_ID;
+  process.env.NODE_ENV === "production" && process.env.YM_ID;
 
 export const YandexMetrica = ({ children }: PropsWithChildren) => {
   const hit = useCallback((url: string) => {

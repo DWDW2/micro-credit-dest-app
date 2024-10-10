bea8e242c6753cf282864375031be94f0b9235db
Zhansar <daid8002@gmail.com>
2024-10-10 16:06:46 +0500

    hero fixed

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="flex items-center justify-center px-4 sm:px-6 lg:px-8 min-h-max mt-14">
         <div className="max-w-3xl text-center leading-tighter tracking-tight">
           <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-gray-900 mb-4">
-            <b className="font-extrabold text-primary">BestOffer.kz</b>{" "}
+            <b className="font-extrabold text-[#9193ff]">BestOffer.kz</b>{" "}
             финансовый маркетплейс
           </h1>
           <p className="text-xl sm:text-2xl text-gray-700 mb-8">
